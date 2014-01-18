577acd79b7c187795e5371da80300ea1b2b62f80
czambel2 <zmbl.soft@gmail.com>
2014-01-18 17:17:35 +0100

    Adds Utility class and redirect methods

---
+++ src/lib/Utility.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Contient diverses fonctions utiles.
+ */
+abstract class Utility {
+	/**
+	 * Redirige vers une URL.
+	 * @param string $url L'URL à rediriger.
+	 */
+	public static function redirect($url) {
+		ob_end_clean();
+
+		$url = preg_replace("#\r|\n#", "", $url);
+
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: " . $url);
+		exit;
+	}
+
+	/**
+	 * Redirige vers une route.
+	 * @param string $route La route vers laquelle on doit rediriger.
+	 * @param array $parameters Les paramètres de la route.
+	 */
+	public static function redirectRoute($route, array $parameters = array()) {
+		self::redirect(Router::generateUrl($route, $parameters));
+	}
+}
