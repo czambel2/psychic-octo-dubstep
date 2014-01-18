390f5f361b824f87738a56c9ca0186c5f9260ce3
czambel2 <zmbl.soft@gmail.com>
2014-01-18 17:16:13 +0100

    Adds error management in LoginForm

--- src/form/LoginForm.php
+++ src/form/LoginForm.php
@@ -8,15 +8,36 @@ class LoginForm extends Form {
 		$this->fields = array('password');
 	}
 
-	public function validate() {
-		if($this->fields['password'] != Config::get('password')) {
+	protected function validate() {
+		if(!array_key_exists('password', $this->data) or $this->data['password'] != Config::get('password')) {
 			$this->addError('password', 'Le mot de passe entré est incorrect.');
 		}
-
-		return $this->isValid();
 	}
 
-	public function getHtml() {
+	public function __toString() {
+		$passwordClass = $this->hasError('password') ? ' class="error"' : null;
+		$passwordMessage = $this->hasError('password') ? '<small class="error">Le mot de passe que vous avez entré est invalide.</small>' : null;
 
+		$html = <<<FORM
+	<div class="row">
+		<input type="hidden" name="form" value="login" />
+
+		<div class="row">
+			<p class="twelve columns">
+				<label for="password">Mot de passe</label>
+				<input type="password" name="data[password]" id="password" autofocus="autofocus"$passwordClass />
+				$passwordMessage
+			</p>
+		</div>
+
+		<div class="row">
+			<p class="twelve columns text-centered">
+				<button type="submit" class="button">Connexion</button>
+			</p>
+		</div>
+	</div>
+FORM;
+
+		return $html;
 	}
 }
