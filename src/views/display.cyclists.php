f7c0d64c52fe53c674248de41026a2f9db965103
czambel2 <zmbl.soft@gmail.com>
2014-01-21 10:07:19 +0100

    Improves rendering and layout of tables

--- src/views/display.cyclists.php
+++ src/views/display.cyclists.php
@@ -1,21 +1,26 @@
-<h1>Liste des Cyclistes</h1>
+<h1>Liste des cyclistes</h1>
 
-<table>
-	<tr>
-		<th>Numéro de Cycliste</th>
-		<th>Nom</th>
-		<th>Prénom</th>
-		<th>Adresse</th>
-		<th>Ville</th>
-	</tr>
-	<?php foreach($cyclistes as $cycliste): ?>
-		<tr>
-			<td><?= $cycliste["numcyc"] ?></td>
-			<td><?= $cycliste["nom"] ?></td>
-			<td><?= $cycliste["prenom"] ?></td>
-			<td><?= $cycliste["adresse"] ?></td>
-			<td><?= $cycliste["ville"] ?></td>
-		</tr>
-	<?php endforeach; ?>
-
-</table>
+<div class="row">
+	<table class="twelve">
+		<thead>
+			<tr>
+				<th>Numéro</th>
+				<th>Nom</th>
+				<th>Prénom</th>
+				<th>Adresse</th>
+				<th>Ville</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($cyclistes as $cycliste): ?>
+				<tr>
+					<td><?= $cycliste["numcyc"] ?></td>
+					<td><?= $cycliste["nom"] ?></td>
+					<td><?= $cycliste["prenom"] ?></td>
+					<td><?= $cycliste["adresse"] ?></td>
+					<td><?= $cycliste["ville"] ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+</div>
--- src/views/race.index.php
+++ src/views/race.index.php
@@ -1,25 +1,30 @@
-<h1>Liste des Courses</h1>
+<h1>Liste des courses</h1>
 
-<table>
-	<tr>
-		<th>Numéro</th>
-		<th>Date</th>
-		<th>Année</th>
-		<th>Nombre de participants</th>
-		<th>Distance C1</th>
-		<th>Distance C2</th>
-		<th>Distance C2</th>
-	</tr>
-	<?php foreach($courses as $course): ?>
-		<tr>
-			<td><?= $course["numcourse"] ?></td>
-			<td><?= $course["datecourse"] ?></td>
-			<td><?= $course["anneecourse"] ?></td>
-			<td><?= $course["nbparticipantstotal"] ?></td>
-			<td><?= $course["distancec1"] ?></td>
-			<td><?= $course["distancec2"] ?></td>
-			<td><?= $course["distancec3"] ?></td>
-		</tr>
-	<?php endforeach; ?>
-
-</table>
+<div class="row">
+	<table class="twelve">
+		<thead>
+			<tr>
+				<th>Numéro</th>
+				<th>Date</th>
+				<th>Année</th>
+				<th>Participants</th>
+				<th>Distance C1</th>
+				<th>Distance C2</th>
+				<th>Distance C2</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($courses as $course): ?>
+				<tr>
+					<td><?= $course["numcourse"] ?></td>
+					<td><?= empty($course["datecourse"]) ? null : (new DateTime($course["datecourse"]))->format('d/m/Y') ?></td>
+					<td><?= $course["anneecourse"] ?></td>
+					<td><?= $course["nbparticipantstotal"] ?></td>
+					<td><?= $course["distancec1"] ?></td>
+					<td><?= $course["distancec2"] ?></td>
+					<td><?= $course["distancec3"] ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+</div>
