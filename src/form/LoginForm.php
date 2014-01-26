6911c85cee3a1e8f4bc0451bca0395a432790e86
czambel2 <zmbl.soft@gmail.com>
2014-01-26 12:18:43 +0100

    Forces password as required in LoginForm

--- src/form/LoginForm.php
+++ src/form/LoginForm.php
@@ -5,6 +5,7 @@
  */
 class LoginForm extends Form {
 	protected function validate() {
+		$this->check()->required('password');
 		$this->check()->equals('password', Config::get('password'), 'Le mot de passe entré est incorrect.');
 	}
 
