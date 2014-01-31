8385341149b2e3f28d99a3c47b060747c2dd2dcf
czambel2 <zmbl.soft@gmail.com>
2014-01-31 14:38:15 +0100

    Improves route handling in Router

--- src/lib/Router.php
+++ src/lib/Router.php
@@ -6,59 +6,65 @@
 abstract class Router {
 
 	/**
+	 * Contient toutes les routes.
+	 *
+	 * C'est un tableau de tableaux.
+	 * @var array(array)
+	 */
+	protected static $routes = array(
+		'home' => array(
+			'index' => '/',
+		),
+		'login' => array(
+			'showForm' => '/connexion',
+			'logout' => '/deconnexion',
+		),
+		'api' => array(
+			'editReward' => '/api/modifier-recompense',
+		),
+		'race' => array(
+			'index' => '/courses/liste',
+			'edit' => '/courses/modifier',
+		),
+		'thisRace' => array(
+			'status' => '/course/etat',
+			'rewards' => '/course/recompenses',
+			'addReward' => '/course/recompenses/ajouter'
+		),
+		'cyclist' => array(
+			'index' => '/cyclistes/liste',
+			'add' => '/cyclistes/ajouter',
+		),
+		'display' => array(
+			'cyclists' => '/impression/liste-cyclistes',
+		),
+
+	);
+
+	/**
 	 * Récupère le contrôleur, l'action et les paramètres associés à une URL donnée.
 	 * @param string $url L'URL demandée par l'utilisateur.
 	 * @return array Un tableau décrivant le contrôleur, l'action et les paramètres.
 	 */
 	public static function parseUrl($url) {
-
 		// Premièrement, on enlève de l'URL à analyser le répertoire de base
 		// Par exemple, si le répertoire de base du site est /lionne/
 		// et que l'on demande l'URL /lionne/accueil, on récupère uniquement "accueil".
 		$url = preg_replace("#^" . preg_quote(Config::get('basePath')) . '#', '', $url);
 		$url = preg_replace("#(\?.+)$#", '', $url);
 
-		$pregParams = array();
 		$parameters = array();
+		$route = null;
 
 		// On analyse l'URL pour voir si elle correspond à une route
-		if(preg_match("#^/$#", $url)) {
-			// Page d'accueil
-			$route = "home.index";
-		} elseif(preg_match("#^/connexion$#", $url)) {
-			// Formulaire de connexion
-			$route = "login.showForm";
-		} elseif(preg_match("#^/deconnexion$#", $url)) {
-			// Déconnexion
-			$route = "login.logout";
-		} elseif(preg_match("#^/impression/liste-cyclistes$#", $url)) {
-			// Liste des cyclistes enregistrés
-			$route = "display.cyclists";
-		} elseif(preg_match("#^/courses/liste$#", $url)) {
-			// Liste des courses enregistrés
-			$route = "race.index";
-		} elseif(preg_match('#^/cycliste/liste$#', $url)) {
-			// Liste des cyclistes
-			$route = "cyclist.index";
-		} elseif(preg_match('#^/cycliste/ajouter$#', $url)) {
-			// Ajouter un cycliste
-			$route = "cyclist.add";
-		} elseif(preg_match('#^/course/etat$#', $url)) {
-			// État de la course
-			$route = "thisRace.status";
-		} elseif(preg_match('#^/course/recompenses$#',$url)) {
-			// Liste des récompenses
-			$route = "thisRace.rewards";
-		} elseif(preg_match("#^/api/modifier-recompense$#", $url)) {
-			// API : Modifier une récompense
-			$route = "api.editReward";
-		} elseif(preg_match("#^/course/recompense/ajout$#", $url)) {
-			// Ajout d'une récompense
-			$route = "thisRace.addReward";
-		} elseif(preg_match("#^/courses/modifier$#", $url)) {
-			// Modification d'une course
-			$route = "race.edit";
-		} else {
+		foreach(self::$routes as $catName => $category) {
+			$search = array_search($url, $category);
+			if(false !== $search) {
+				$route = $catName . "." . $search;
+			}
+		}
+
+		if(!$route) {
 			// L'URL ne correspond à aucune route : on lève une exception
 			throw new NoRouteForUrlException($url);
 		}
@@ -76,6 +82,7 @@ abstract class Router {
 		$returnValue = array();
 
 		$controllerAndAction = explode('.', $route);
+		$returnValue["controllerShort"] = $controllerAndAction[0];
 		$returnValue["controller"] = lcfirst($controllerAndAction[0] . "Controller");
 		$returnValue["action"] = $controllerAndAction[1];
 
@@ -85,52 +92,15 @@ abstract class Router {
 	}
 
 	public static function generateUrl($route, array $parameters = array()) {
-
 		$url = Config::get('basePath');
 
-		switch($route) {
-			case 'home.index':
-				$url = '/';
-				break;
-			case 'login.showForm':
-				$url = '/connexion';
-				break;
-			case 'login.logout':
-				$url = '/deconnexion';
-				break;
-			case 'display.cyclists':
-				$url = '/impression/liste-cyclistes';
-				break;
-			case 'race.index' :
-				$url = '/courses/liste';
-				break;
-			case 'cyclist.index':
-				$url = '/cycliste/liste';
-				break;
-			case 'cyclist.add':
-				$url = '/cycliste/ajouter';
-				break;
-			case 'thisRace.status':
-				$url = '/course/etat';
-				break;
-			case 'thisRace.rewards':
-				$url = '/course/recompenses';
-				break;
-			case 'api.editReward':
-				$url = '/api/modifier-recompense';
-				break;
-			case 'thisRace.addReward':
-				$url = '/course/recompense/ajout';
-				break;
-			case 'race.edit':
-				$url = '/courses/modifier';
-				break;
-			default:
-				throw new RouterException($route);
+		$returnValue = self::parseRoute($route, $parameters);
+
+		if(array_key_exists($returnValue["controllerShort"], self::$routes) and array_key_exists($returnValue["action"], self::$routes[$returnValue["controllerShort"]])) {
+			return $url . self::$routes[$returnValue["controllerShort"]][$returnValue["action"]];
+		} else {
+			throw new RouterException($route);
 		}
-
-		return $url;
-
 	}
 
 }
