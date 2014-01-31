3ee4e4ee9d1a5ab7a41e00a2e6d4aa4fc1079cc1
Sinthalol <g.dorlhac@gmail.com>
2014-01-31 15:17:51 +0100

    Changes cyclist.index URL

--- src/views/cyclist.index.php
+++ src/views/cyclist.index.php
@@ -20,7 +20,7 @@
 					<td><?= e($cyclist["prenom"]) ?></td>
 					<td><?= e($cyclist["adresse"]) ?></td>
 					<td><?= e($cyclist["ville"]) ?></td>
-					<td class="text-center"><a class="edit-button" title="Modifier" href="<?= url('cyclist.edit') ?>?id=<?= $cyclist["numcyc"] ?>">Modifier</a></td>
+					<td class="text-center"><a class="edit-button" title="Modifier" href="<?= url('cyclist.edit', array("id" => $cyclist["numcyc"])) ?>">Modifier</a></td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
