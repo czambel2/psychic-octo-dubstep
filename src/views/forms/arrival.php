70e06c5d26224a5c914c422d623ed662b150ebcd
czambel2 <zmbl.soft@gmail.com>
2014-02-10 13:33:03 +0100

    Cosmetic fix on arrival form

--- src/views/forms/arrival.php
+++ src/views/forms/arrival.php
@@ -3,7 +3,7 @@
 		<label for="data_cyclistName" class="right inline required">Coureur&nbsp;:</label>
 	</div>
 	<div class="nine columns">
-		<input data-race-number="<?= $this->getMiscData('raceNumber') ?>" name="data[cyclistName]" class="autocomplete-cyclists-filter arrival" required="required" id="data_cyclistName" placeholder="Rechercher un cycliste" type="text" <?= $this->errorClass('cyclistName') ?> />
+		<input data-race-number="<?= $this->getMiscData('raceNumber') ?>" name="data[cyclistName]" class="autocomplete-cyclists-filter arrival <?= $this->errorClass('cyclistName', true) ?>" required="required" id="data_cyclistName" placeholder="Rechercher un cycliste" type="text" />
 		<?= $this->errorMessage('cyclistName') ?>
 		<div class="autocomplete-results"></div>
 	</div>
